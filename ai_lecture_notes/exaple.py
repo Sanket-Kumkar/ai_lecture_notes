56b13796b20ef4100270250b13dc6c1c925c058c
kumkar sanket <154042184+Sanket-Kumkar@users.noreply.github.com>
2026-01-07 11:53:37 +0530

    Delete ai_lecture_notes/exaple.py

--- ai_lecture_notes/exaple.py
+++
@@ -1,6 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyAB9tUMDK9kz2lRwJ_4ebXdDym8T3A7I-8")
-
-for m in genai.list_models():
-    print(m.name, m.supported_generation_methods)
